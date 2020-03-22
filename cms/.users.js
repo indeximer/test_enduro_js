b7ffdfe02493ed9d882f0af8303b707692a52b1f
Douglas <douglasfortunato@hotmail.com>
2020-03-22 12:53:47 -0300

    initial commit.

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'indeximer',
+			tags: [],
+			salt: '5a211b160fdb3c39b8413dde5142f9be',
+			hash: '81cd1075757cc15074a071c554895e3df9abc8cdd562ef6b750cc52f5876d07b',
+			user_created_timestamp: 1584892012509
+		}
+	]
+})
